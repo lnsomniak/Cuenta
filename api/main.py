b6c01220e1b8d3c42ab4ddfc6eacc570aef3b7ef
Sergio Hernandezgit config --global user.email <tinyminion83@gmail.com>
2025-12-14 20:09:18 -0600

    ...

--- api/main.py
+++ api/main.py
@@ -26,6 +26,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://cuenta-mu.vercel.app",
         os.getenv("FRONTEND_URL", "")
     ],
     allow_credentials=True,
